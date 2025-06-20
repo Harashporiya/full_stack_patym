654f4df5719ee4ece1abe8dd5101048b496417cf
harash <harash.poriya@gmail.com>
2025-06-20 08:58:05 +0530

    create transaction model

---
+++ backend/model/transaction.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const transactionSchema = new Schema({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending'
+    }
+}, { timestamps: true });
+
+const Transaction = model("Transaction", transactionSchema);
+module.exports = Transaction;
