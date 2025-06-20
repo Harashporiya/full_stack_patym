20360c6283d02cc94a6f0f041dcc32804a71ca3a
harash <harash.poriya@gmail.com>
2025-06-20 09:00:59 +0530

    deleted file

--- backend/model/userSendMoney.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-const TransactionSchema = new mongoose.Schema({
-    users:[String]
-},{timestamps:true})
-
-const Transaction = mongoose.model("Transaction", TransactionSchema)
-module.exports =Transaction;
