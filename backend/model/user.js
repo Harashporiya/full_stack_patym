4ea05963030e9a9082b94d19c0ac78888b3b0d45
harash <harash.poriya@gmail.com>
2025-06-20 08:58:29 +0530

    model user

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,25 +1,28 @@
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    firstname:{
-        type:String,
+    firstname: {
+        type: String,
         required: true,
     },
-    lastname:{
-        type:String,
+    lastname: {
+        type: String,
         required: true,
     },
-    email:{
-        type:String,
+    email: {
+        type: String,
         required: true,
         unique: true,
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         required: true,
+    },
+    balance: {
+        type: Number,
+        default: 0
     }
-})
+});
 
 const User = model("User", userSchema);
-
 module.exports = User;
